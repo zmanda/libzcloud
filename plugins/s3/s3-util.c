954f9e659c7ef4a7108c961c7f02e47856ed56c1
Dustin J. Mitchell <dustin@zmanda.com>
2009-09-28 14:45:21 -0400

    replace copyright header

--- plugins/s3/s3-util.c
+++ plugins/s3/s3-util.c
@@ -1,22 +1,20 @@
-/*
- * Copyright (c) 2005-2008 Zmanda Inc.  All Rights Reserved.
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vi: set tabstop=4 shiftwidth=4 expandtab: */
+/*  ***** BEGIN LICENSE BLOCK *****
+ * Copyright (C) 2009 Zmanda Incorporated. All Rights Reserved.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License version 2.1 as
- * published by the Free Software Foundation.
+ * This file is part of libzcloud.
  *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
+ * libzcloud is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (the LGPL)
+ * as published by the Free Software Foundation, either version 2.1 of
+ * the LGPL, or (at your option) any later version.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
- *
- * Contact information: Zmanda Inc., 465 S Mathlida Ave, Suite 300
- * Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
- */
+ * libzcloud is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *  ***** END LICENSE BLOCK ***** */
 
 
 #ifdef HAVE_CONFIG_H
